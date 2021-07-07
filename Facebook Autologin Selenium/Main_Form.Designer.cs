49b1f4eb082274aa5c53ebf6702dd5564cd4913b
FarhanMaulanaF <farhanmaulanafirmansyah@gmail.com>
2021-07-07 13:31:22 +0700

    Commit Stage

---
+++ Facebook Autologin Selenium/Main_Form.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Facebook_Autologin_Selenium
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.White;
+            this.lblUsername.Location = new System.Drawing.Point(108, 92);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(150, 29);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "USERNAME";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(282, 89);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(280, 35);
+            this.tbUsername.TabIndex = 1;
+            this.tbUsername.TextChanged += new System.EventHandler(this.TextBoxChanged);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(282, 148);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(280, 35);
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.UseSystemPasswordChar = true;
+            this.tbPassword.TextChanged += new System.EventHandler(this.TextBoxChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(108, 151);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(151, 29);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "PASSWORD";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Red;
+            this.btnLogin.Location = new System.Drawing.Point(222, 239);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(203, 53);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Main_Form
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(651, 360);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main_Form";
+            this.Text = "Facebook Selenium Login";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_Form_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Facebook Autologin Selenium/Main_Form.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Facebook_Autologin_Selenium
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        ChromeDriver drv; Thread th;
+        private void TextBoxChanged(object sender, EventArgs e)
+        {
+            if (tbUsername.Text != "" && tbPassword.Text != "")
+            {
+                btnLogin.ForeColor = Color.Lime;
+                btnLogin.Cursor = Cursors.Hand;
+            }
+            else
+            {
+                btnLogin.ForeColor = Color.Red;
+                btnLogin.Cursor = Cursors.No;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (btnLogin.Cursor == Cursors.Hand)
+            {
+                th = new Thread(Result); th.Start();
+            }
+        }
+
+        private void Result()
+        {
+            btnLogin.ForeColor = Color.Gold;
+            btnLogin.UseWaitCursor = true;
+            btnLogin.Text = "Testing...";
+            OpenSelenium();
+            Thread.Sleep(3000);
+            Login(tbUsername.Text, tbPassword.Text);
+            if (TestAccount())
+                MessageBox.Show("Login Successfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Username or Password is wrong!","Warning!",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            
+            CloseSelenium();
+            btnLogin.ForeColor = Color.Lime;
+            btnLogin.UseWaitCursor = false;
+            btnLogin.Text = "Login";
+        }
+
+        private void Login(string username, string password)
+        {
+            try
+            {
+                drv.FindElements(By.XPath("//input[@class='inputtext _55r1 inputtext _1kbt inputtext _1kbt']"))[0].SendKeys(username); Thread.Sleep(3000);
+                drv.FindElements(By.XPath("//input[@class='inputtext _55r1 inputtext _9npi inputtext _9npi']"))[0].SendKeys(password); Thread.Sleep(3000);
+                drv.FindElement(By.XPath("//button[@class='_42ft _4jy0 _52e0 _4jy6 _4jy1 selected _51sy']")).Click(); Thread.Sleep(1000);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private bool TestAccount()
+        {
+            if (drv.Url == "https://www.facebook.com/login")
+                return true;
+            else
+                return false;
+        }
+
+        private void OpenSelenium()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            drv = new ChromeDriver(service);
+            drv.Navigate().GoToUrl("https://www.facebook.com/login");
+        }
+
+        private void Main_Form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseSelenium();
+        }
+        private void CloseSelenium()
+        {
+            drv.Quit();
+        }
+    }
+}
